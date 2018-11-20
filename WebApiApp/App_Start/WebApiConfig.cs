77bc684eb370969aa3e8bc0c52af845942bc5671
onyekachila <onyekachila@gmail.com>
2018-11-20 18:06:17 +0100

    Added attribute routing by removing the default routing method in web api config.

--- WebApiApp/App_Start/WebApiConfig.cs
+++ WebApiApp/App_Start/WebApiConfig.cs
@@ -14,17 +14,17 @@ namespace WebApiApp
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-               name: "ProdApi",
-               routeTemplate: "api/prod/{id}",
-               defaults: new { controller = "products", id = RouteParameter.Optional }
-           );
+           // config.Routes.MapHttpRoute(
+           //    name: "ProdApi",
+           //    routeTemplate: "api/prod/{id}",
+           //    defaults: new { controller = "products", id = RouteParameter.Optional }
+           //);
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+           // config.Routes.MapHttpRoute(
+           //     name: "DefaultApi",
+           //     routeTemplate: "api/{controller}/{id}",
+           //     defaults: new { id = RouteParameter.Optional }
+           // );
         }
     }
 }
--- WebApiApp/Controllers/ProductsController.cs
+++ WebApiApp/Controllers/ProductsController.cs
@@ -7,32 +7,44 @@ using System.Web.Http;
 
 namespace WebApiApp.Controllers
 {
+    [RoutePrefix("products")]
     public class ProductsController : ApiController
     {
         // GET: api/Products
-        public IEnumerable<string> GetAllProducts()
+        [HttpGet, Route("")]
+        public IEnumerable<string> ReturnAllProducts()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "product 1", "product 2" };
         }
 
         // GET: api/Products/5
+        [HttpGet, Route("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST: api/Products
-        [HttpPost]
+        // GET: api/Products/5/order/custid
+        [HttpGet, Route("{id}/orders/custid")]
+        public string Get(int id, string custid)
+        {
+            return "product-orders " + custid;
+        }
+
+        // POST: api/products/
+        [HttpPost, Route("")]
         public void CreateProduct([FromBody]string value)
         {
         }
 
         // PUT: api/Products/5
+        [HttpPut, Route("id")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
         // DELETE: api/Products/5
+        [HttpDelete, Route("id")]
         public void Delete(int id)
         {
         }
